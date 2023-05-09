658365c4f3a30a16fba2b4f55f2bc7fa290e6826
nilsherzig <nilsdev+1@protonmail.com>
2023-05-09 20:24:07 +0000

    nothing here main page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,11 @@
-/** @type {import('./$types').PageLoad} */
-export async function load({ fetch, params }) {
-    try {
-    const res = await fetch('http://localhost:3000/healthcheck');
-    const item = await res.json();
-        return { item };
-    } catch(error) {
-        const item = await JSON.parse('{"state":"offline"}');
-        return { item };
-    }
-}
+// /** @type {import('./$types').PageLoad} */
+// export async function load({ fetch, params }) {
+//     try {
+//     const res = await fetch('http://localhost:3000/healthcheck');
+//     const item = await res.json();
+//         return { item };
+//     } catch(error) {
+//         const item = await JSON.parse('{"state":"offline"}');
+//         return { item };
+//     }
+// }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	/** @type {import('./$types').PageData} */
-	import Spinner from '$lib/spinner.svelte';
-	import RenderMarkdown from '../lib/renderMarkdown.svelte';
-	export let data;
+	// /** @type {import('./$types').PageData} */
+	// import Spinner from '$lib/spinner.svelte';
+	// import RenderMarkdown from '../lib/renderMarkdown.svelte';
+	// export let data;
 </script>
 
 <div class="container mx-auto mt-4 text-zinc-200">
@@ -10,5 +10,5 @@
 	<!-- <a href="/math" class="text-blue-500 underline hover:cursor-pointer">Mathe rsa thing</a>
 
 	<p>Backend check:</p> -->
-	<RenderMarkdown data={data.item} />
+	<!-- <RenderMarkdown data={data.item} /> -->
 </div>
